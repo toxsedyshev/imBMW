1a5a53947cc95b29b0fb61d8e76dfc6e9f799209
Anton Sedyshev <me@toxs.ru>
2015-01-22 19:12:43 +0300

    PCL logger implemented

--- Sources/NET/imBMW.Portable/Tools/Logger.cs
+++ Sources/NET/imBMW.Portable/Tools/Logger.cs
@@ -7,11 +7,45 @@ using System.Threading.Tasks;
 
 namespace imBMW
 {
+    public enum LogPriority
+    {
+        Info, 
+        Debug,
+        Warning,
+        Error
+    }
+
+    public class LogItem
+    {
+        public DateTime Timestamp { get; set; }
+
+        public LogPriority Priority { get; set; }
+
+        public string Message { get; set; }
+
+        public string PriorityLabel { get; set; }
+
+        public Exception Exception { get; set; }
+
+        public LogItem()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+
     public class Logger
     {
+        public static event Action<LogItem> Logged;
+
         public static void Error(Exception ex, string message)
         {
-            Debug.WriteLine(DateTime.Now.ToString() + " [ERR] " + message + ": " + ex.Message + " Stack trace:\n" + ex.StackTrace);
+            OnLogged(new LogItem
+            {
+                Priority = LogPriority.Error,
+                PriorityLabel = "ERR",
+                Message = message,
+                Exception = ex
+            });
         }
 
         public static void Info(string message, string priority = null)
@@ -20,7 +54,21 @@ namespace imBMW
             {
                 priority = "i";
             }
-            Debug.WriteLine(DateTime.Now.ToString() + " [" + priority + "] " + message);
+            OnLogged(new LogItem
+            {
+                Priority = LogPriority.Info,
+                PriorityLabel = "i",
+                Message = message
+            });
+        }
+
+        static void OnLogged(LogItem item)
+        {
+            var e = Logged;
+            if (e != null)
+            {
+                e(item);
+            }
         }
     }
 }
