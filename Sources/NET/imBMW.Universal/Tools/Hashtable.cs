1ff40c85b92d12670b61c2e17273ce7d6a831d41
Anton Sedyshev <me@toxs.ru>
2016-08-15 12:58:00 +0300

    UWP Hashtable fix.

---
+++ Sources/NET/imBMW.Universal/Tools/Hashtable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imBMW.Tools
+{
+    public class Hashtable : Dictionary<object, object>
+    {
+        public bool Contains(string key)
+        {
+            return ContainsKey(key);
+        }
+    }
+}
