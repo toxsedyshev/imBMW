dc604356e6970a8f3e7eb04c7254aa0180a85515
Anton Sedyshev <me@toxs.ru>
2016-08-15 12:27:57 +0300

    JMGarage AFR formula changed.

--- Sources/NET-MF/imBMW.Diagnostics/DME/MS43JMGAnalogValues.cs
+++ Sources/NET-MF/imBMW.Diagnostics/DME/MS43JMGAnalogValues.cs
@@ -23,7 +23,7 @@ namespace imBMW.Diagnostics.DME
             var d = message.Data;
             LambdaHeatingAfterCats1 = -1; // byte #30 is used for AFR instead
             LambdaHeatingAfterCats2 = -1; // byte #31 is used for IntakePressure instead
-            WideBandLambda = d[30] * 0.0009914 - 31.98155; // TODO ??? fix
+            WideBandLambda = (double)d[30] / 255 + 0.5; // TODO fix - not JMG formula!
             AFR = WideBandLambda * 14.7;
             IntakePressure = d[31] * 10; // 0..2550 hPa
         }
@@ -31,9 +31,14 @@ namespace imBMW.Diagnostics.DME
         public static DBusMessage ModifyMS43Message(DMEAnalogValues av, Message message)
         {
             var data = message.Data.Skip(0);
-            data[30] = (byte)((av.WideBandLambda - 0.5) * 255); // TODO why not?
-            data[31] = (byte)(av.IntakePressure / 10);
+            data[30] = ToByte((av.WideBandLambda - 0.5) * 255); // TODO why not? TBD with JMG
+            data[31] = ToByte(av.IntakePressure / 10);
             return new DBusMessage(DeviceAddress.DME, message.ReceiverDescription, data);
+        }
+
+        static byte ToByte(double d)
+        {
+            return (byte)System.Math.Min(255, System.Math.Max(0, d));
         }
     }
 }
