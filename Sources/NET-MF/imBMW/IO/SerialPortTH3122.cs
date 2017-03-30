b1e1a1f0155718e6d509454598c55e4b5fdbe063
Anton Sedyshev <me@toxs.ru>
2017-03-30 20:48:39 +0300

    Cerberus parity bug fixed in the firmware. Finally.

--- Sources/NET-MF/imBMW/IO/SerialPortTH3122.cs
+++ Sources/NET-MF/imBMW/IO/SerialPortTH3122.cs
@@ -6,6 +6,12 @@ namespace System.IO.Ports
 {
     public class SerialPortTH3122 : SerialInterruptPort
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="port"></param>
+        /// <param name="busy"></param>
+        /// <param name="fixParity">Set true if the data is corrupted because of parity bit issue in Cerberus software.</param>
         public SerialPortTH3122(String port, Cpu.Pin busy, bool fixParity = false) :
             base(new SerialPortConfiguration(port, BaudRate.Baudrate9600, Parity.Even, 8 + (fixParity ? 1 : 0), StopBits.One), busy, 0, imBMW.iBus.Message.PacketLengthMax, 50)
         {
